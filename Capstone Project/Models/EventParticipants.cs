5828239b4f9606facfbb247ab388633598aafab6
Don-Uherka <uherkadon@yahoo.com>
2020-11-23 14:15:32 -0600

    i created the EventPaticipants model and the Post model

---
+++ Capstone Project/Models/EventParticipants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Project.Models
+{
+    public class EventParticipants
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("EventId")]
+        public int EventId { get; set; }
+
+        [ForeignKey("ParticipantId")]
+        public int ParticipantId { get; set; }
+    }
+}
---
+++ Capstone Project/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Project.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool Anonymous { get; set; }
+        public string Content { get; set; }
+        [ForeignKey("ParticipantId")]
+        public string participantId { get; set; }
+    }
+}
